50640c562ed907deb33dd23f2a8702fa9cb5caf5
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-11-09 03:59:37 -0500

    WIP

---
+++ pages/Moralis/index.tsx
@@ -0,0 +1,17 @@
+import "../../styles/App.module.css";
+import Header from "@/components/Header";
+import Agents from "@/components/Agents";
+import Recipients from "@/components/Recipients";
+import Swap from "@/components/Swap";
+import Tokens from "@/components/Tokens";
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ pages/Moralis2/index.tsx
@@ -0,0 +1,36 @@
+import "../../styles/App.module.css";
+import Header from "@/components/Header";
+import Agents from "@/components/Agents";
+import Recipients from "@/components/Recipients";
+import Swap from "@/components/Swap";
+import Tokens from "@/components/Tokens";
+
+// import { Routes, Route } from "react-router-dom";
+// import { useConnect, useAccount } from "wagmi";
+// import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+
+function App() {
+  // const { address, isConnected } = useAccount();
+  // const { connect } = useConnect({
+  //   connector: new MetaMaskConnector(),
+  // });
+
+  return (
+    <h1>Hello World</h1>
+
+    // <div className="App">
+    //   <Header connect={connect} isConnected={isConnected} address={address} />
+    //   <div className="mainWindow">
+    //     <Routes>
+    //       <Route path="/" element={<Swap isConnected={isConnected} address={address} />} />
+    //       <Route path="/Agents" element={<Agents />} />
+    //       <Route path="/tokens" element={<Tokens />} />
+    //       <Route path="/Recipients" element={<Recipients />} />
+    //     </Routes>
+    //   </div>
+
+    // </div>
+  )
+}
+
+// export default App;
